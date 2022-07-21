f7522175f62d96a48a6031b68fbb5eaebc26e3df
ashokkarad4 <ashokkarad4@gmail.com>
2022-07-21 12:57:29 +0530

    Add project files.

---
+++ CRUDAssingmentUsingADO/Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using CRUDAssingmentUsingADO.DAL;
+using CRUDAssingmentUsingADO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAssingmentUsingADO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDAL db = new EmployeeDAL();
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var model = db.GetAllEmployee();
+            return View(model);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = db.GetEmployeeById(id);
+            return View(model);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                int result = db.AddEmployee(employee);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = db.GetEmployeeById(id);
+            return View(model);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee)
+        {
+            try
+            {
+                int result = db.UpdateEmployee(employee);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = db.GetEmployeeById(id);
+            return View(model);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                int result = db.DeleteEmployee(id);
+                if (result == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDAssingmentUsingADO/DAL/EmployeeDAL.cs
@@ -0,0 +1,98 @@
+﻿using CRUDAssingmentUsingADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAssingmentUsingADO.DAL
+{
+    public class EmployeeDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public EmployeeDAL()
+        {
+            con = new SqlConnection(Startup.ConnectinString);
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            List<Employee> plist = new List<Employee>();
+            string qry = "select * from Employee";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Employee p = new Employee();
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Salary = Convert.ToDouble(dr["Salary"]);
+                    plist.Add(p);
+                }
+            }
+            con.Close();
+            return plist;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            Employee p = new Employee();
+            string qry = "select * from Employee where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Salary = Convert.ToDouble(dr["Salary"]);
+                }
+            }
+            con.Close();
+            return p;
+        }
+
+        public int AddEmployee(Employee prod)
+        {
+            string qry = "insert into Employee values(@name,@price)";
+            cmd = new SqlCommand(qry, con);
+
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@salary", prod.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int UpdateEmployee(Employee prod)
+        {
+            string qry = "update Employee set Name=@name , Salary=@salary where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", prod.Id);
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@salary", prod.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int DeleteEmployee(int id)
+        {
+            string qry = "delete from Employee where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+    }
+}
+
